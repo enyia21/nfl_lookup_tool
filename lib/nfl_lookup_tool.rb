8d52e29203903a6103b529ec6793fabb2a765036
chibuisi <enyia21@gmail.com>
2020-05-08 11:57:28 -0500

    added description

---
+++ lib/nfl_lookup_tool.rb
@@ -0,0 +1,6 @@
+require "nfl_lookup_tool/version"
+
+module NflLookupTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nfl_lookup_tool/version.rb
@@ -0,0 +1,3 @@
+module NflLookupTool
+  VERSION = "0.1.0"
+end
